4f8a4d171f0caeb7e4f79eab030312b5e7cad2a8
John <robinsonkukuruzo@gmail.com>
2023-11-27 09:15:44 -0800

    Add extra styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
     const phoneContainer = document.querySelector('#root');
-
+    let topFive = 5;
     phones.forEach(element => {
         let description = element.snippet.substring(0, 90);
         description += '. . .';
@@ -10,19 +10,26 @@ window.addEventListener('DOMContentLoaded', () => {
         const left_col = document.createElement('div');
         left_col.classList.add('col-3');
         const right_col = document.createElement('div');
-        right_col.classList.add('col-6', 'align-items-center');
+        right_col.classList.add('col-6');
         const phoneImage = document.createElement('img');
         phoneImage.src = element.imageUrl;
         phoneImage.alt = `${element.name}`;
         phoneImage.classList.add('rounded', 'phone-image', 'object-fit-contain');
         left_col.append(phoneImage);
+        const newSpan = document.createElement('span');
+        newSpan.textContent = 'New';
+        newSpan.classList.add('badge', 'text-bg-success')
         const phoneName = document.createElement('h5');
         phoneName.textContent = element.name;
+        if (topFive > 0) {
+            phoneName.append(newSpan);
+            topFive -= 1;
+        }
         const phoneDesc = document.createElement('p');
         phoneDesc.classList.add('text-center', 'col');
         phoneDesc.textContent = `${description}`;
         right_col.append(phoneName, phoneDesc);
-        item.append(left_col, right_col)
+        item.append(left_col, right_col);
         phoneContainer.append(item);
 
     });
